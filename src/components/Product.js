00fb4a65f2af4089c3aad07d65d50a49215cb5e5
sk-result <jasasatu703@gmail.com>
2025-06-18 22:10:46 +0700

    commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -186,7 +186,7 @@ export default function Product() {
 
 export function Product1() {
   return (
-    <div className="bg-none">
+    <div className="bg-non">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
