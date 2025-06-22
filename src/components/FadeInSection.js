0a9dc94f0c72497ca85756d9a326d42bff179e6b
sk-result <jasasatu703@gmail.com>
2025-06-22 19:40:54 +0700

    commit

--- src/components/FadeInSection.js
+++ src/components/FadeInSection.js
@@ -1,56 +1,56 @@
-// "use client";
-// import { useEffect, useRef, useState } from "react";
+"use client";
+import { useEffect, useRef, useState } from "react";
 
-// export default function FadeInSection({ children, direction = "up" }) {
-//   const ref = useRef();
-//   const [isVisible, setVisible] = useState(false);
+export default function FadeInSection({ children, direction = "up" }) {
+  const ref = useRef();
+  const [isVisible, setVisible] = useState(false);
 
-//   useEffect(() => {
-//     const observer = new IntersectionObserver(
-//       ([entry]) => {
-//         setVisible(entry.isIntersecting);
-//       },
-//       { threshold: 0.1 }
-//     );
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 }
+    );
 
-//     if (ref.current) {
-//       observer.observe(ref.current);
-//     }
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
 
-//     return () => observer.disconnect();
-//   }, []);
+    return () => observer.disconnect();
+  }, []);
 
-//   // Tentukan class animasi
-//   let animationClass = "";
-//   switch (direction) {
-//     case "in":
-//       animationClass = "animate-fadeIn";
-//       break;
-//     case "left":
-//       animationClass = "animate-fadeInLeft";
-//       break;
-//     case "left":
-//       animationClass = "animate-fadeInLeft";
-//       break;
-//     case "right":
-//       animationClass = "animate-fadeInRight";
-//       break;
-//     case "down":
-//       animationClass = "animate-fadeInDown";
-//       break;
-//     case "up":
-//     default:
-//       animationClass = "animate-fadeInUp";
-//   }
+  // Tentukan class animasi
+  let animationClass = "";
+  switch (direction) {
+    case "in":
+      animationClass = "animate-fadeIn";
+      break;
+    case "left":
+      animationClass = "animate-fadeInLeft";
+      break;
+    case "left":
+      animationClass = "animate-fadeInLeft";
+      break;
+    case "right":
+      animationClass = "animate-fadeInRight";
+      break;
+    case "down":
+      animationClass = "animate-fadeInDown";
+      break;
+    case "up":
+    default:
+      animationClass = "animate-fadeInUp";
+  }
 
-//   return (
-//     <div
-//       ref={ref}
-//       className={`transition-opacity duration-700 ease-in-out ${
-//         isVisible ? `opacity-100 ${animationClass}` : "opacity-0"
-//       }`}
-//     >
-//       {children}
-//     </div>
-//   );
-// }
+  return (
+    <div
+      ref={ref}
+      className={`transition-opacity duration-700 ease-in-out ${
+        isVisible ? `opacity-100 ${animationClass}` : "opacity-0"
+      }`}
+    >
+      {children}
+    </div>
+  );
+}
