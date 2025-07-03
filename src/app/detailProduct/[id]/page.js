885fe3dbfdd2a5c70591a587df1eab0163fa5733
sk-result <jasasatu703@gmail.com>
2025-07-03 21:17:02 +0700

    detail product

--- src/app/detailProduct/[id]/page.js
+++ src/app/detailProduct/[id]/page.js
@@ -1,9 +1,9 @@
-// "use client";
-// import { useParams } from "next/navigation";
-// import ProductDetail from "../../../components/ProductDetail"; // Komponen kamu yang sudah dibuat
+"use client";
+import { useParams } from "next/navigation";
+import ProductDetail from "../../../components/ProductDetail"; // Komponen kamu yang sudah dibuat
 
-// export default function DetailProductPage() {
-//   const { id } = useParams();
+export default function DetailProductPage() {
+  const { id } = useParams();
 
-//   return <ProductDetail id={id} />;
-// }
+  return <ProductDetail id={id} />;
+}
