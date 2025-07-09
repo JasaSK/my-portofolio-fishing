1d3df7837715bd27e0a18e93ce2ac2d33c2d37f9
sk-result <jasasatu703@gmail.com>
2025-07-09 09:21:29 +0700

    axios.js update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://your-backend-production.up.railway.app/api",
+  baseURL: "https://https://my-portofolio-backend-fishing-production.up.railway.app//api",
   withCredentials: true,  
 });
 
